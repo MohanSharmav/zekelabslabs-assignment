e1a2fd21831f24f698a4e2c24a32cde4f921aca9
MohanSharmav <45537890+MohanSharmav@users.noreply.github.com>
2020-01-05 19:20:40 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  5 07:00:43 2020
+
+@author: mohan
+
+class retired:
+    # defining the class
+     names=input(sting())
+     couple=input(boolean())  
+     
+    
+    def allocatefund(self,fund):
+    #defining funtion alocate funds
+    self.fund=fund
+    
+    
+    def fund(self):
+        return self.fund
+
+    def __str9__(self):
+        return "%s is a %s" % (self.fund)
+
+    
+    def jointhemself(self,name,couple)
+    #defining function jointemself
+     self.name=namevv 
+     self.couple=couple
+     
+     
+    def name(self):
+        return self.name
+
+    def couple(self):
+        return self.couple
+
+    def __str8__(self):
+        return "%s is a %s" % (self.name, self.couple)
+
+     def approve(self,accept):
+     #define approve function
+     self.accept=accept
+
+      
+    def accept(self):
+        return self.accept
+
+    def __str7__(self):
+        return "%s is a %s" % (self.accept)
+ 
+
+class young: 
+    name=string(input())
+    gender=string(input())
+    
+    #define young class
+    
+    def earn(self,money):
+     self.money=moeny
+  
+    
+    def money(self):
+        return self.moeny
+
+
+    def __str6__(self):
+        return "%s is a %s" % (self.moeney)
+        
+    def care(self,care)
+       self.care=care
+       
+         def care(self):
+        return self.care
+
+
+    def __str5__(self):
+        return "%s is a %s" % (self.care) 
+        
+        
+    def request(self,request)
+         self.request=request
+         
+        def request(self):
+        return self.request
+
+
+    def __str4__(self):
+        return "%s is a %s" % (self.request)    
+        
+        
+    
+    def request(self,choosing)
+         self.choosing=choosing
+         
+        def request(self):
+        return self.choosing
+
+
+    def __str3__(self):
+        return "%s is a %s" % (self.choosing)
+
+
+    
+    def request(self,max):
+         self.max=max
+         
+        def request(self):
+        return self.max
+
+
+    def __str2__(self):
+        return "%s is a %s" % (self.max) 
+        
+        
+class review ():
+   rate=int(input())
+   review=string(input())
+    
+    def rate(self,rate):
+         self.rate=rate
+         
+        def rate(self):
+        return self.rate
+
+
+    def __str1_(self):
+        returq"n "%s is a %s" % (self.rate) 
+        
+        
+        
+    def review(self,review):
+         self.review=review
+         
+        def review(self):
+        return self.review
+
+
+    def __str10__(self):
+        return "%s is a %s" % (self.review) 
+        
+        
+class info:
+    
+    combi=input(string())
+    
+    def combi(self,combi):
+         self.combi=combi
+         
+        def request(self):
+        return self.combi
+
+
+    def __str14__(self):
+        return "%s is a %s" % (self.max) 
+        
+        
+ 
---
+++ showing.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  5 15:16:41 2020
+
+@author: rohit
+"""
+
+#funtion to show who is taking care of older couple
+ 
+def info():
+    
+    while(couple=true)
+    for name in young :
+        print(name in young is taking care of , name in retired couples)
+        
+  
+def chap():
+    
+    while(gender=male)
+    for name in retired:
+        print(name in retied is taken care by, name in young)
